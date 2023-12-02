4953d9c525930bbd78414a5cc0ae921f24c154d2
Rozeri Dilar <rozeri@journiapp.com>
2023-12-02 18:07:48 +0100

    Add Day 2 Part 2

--- Day2/Day2.playground/Contents.swift
+++ Day2/Day2.playground/Contents.swift
@@ -40,6 +40,39 @@ func sumOfPossibleGameIDs(lines: [String]) -> Int {
 }
 
 
+func minSetOfCubesPowerSum(lines: [String]) -> Int {
+    var powerSum = 0
+
+    for line in lines where !line.isEmpty {
+        let parts = line.components(separatedBy: ": ")
+        guard parts.count == 2 else {
+            continue
+        }
+
+        let draws = parts[1].components(separatedBy: "; ")
+        var maxRed = 0, maxGreen = 0, maxBlue = 0
+
+        for draw in draws {
+            let counts = draw.components(separatedBy: ", ").reduce(into: [String: Int]()) { acc, colorCount in
+                let details = colorCount.components(separatedBy: " ")
+                if let count = Int(details[0]) {
+                    let color = details[1]
+                    acc[color, default: 0] = max(acc[color, default: 0], count)
+                }
+            }
+
+            maxRed = max(maxRed, counts["red", default: 0])
+            maxGreen = max(maxGreen, counts["green", default: 0])
+            maxBlue = max(maxBlue, counts["blue", default: 0])
+        }
+
+        powerSum += maxRed * maxGreen * maxBlue
+    }
+
+    return powerSum
+}
+
+
 func readFileContent(filename: String) -> String? {
     if let fileURL = Bundle.main.url(forResource: filename, withExtension: "txt") {
         do {
@@ -55,8 +88,10 @@ func readFileContent(filename: String) -> String? {
 let filename = "Input"
 if let fileContent = readFileContent(filename: filename) {
     let lines = fileContent.components(separatedBy: .newlines)
-    let result = sumOfPossibleGameIDs(lines: lines)
-    print("Sum of possible game IDs:", result)
+    let resultPart1 = sumOfPossibleGameIDs(lines: lines)
+    let resultPart2 = minSetOfCubesPowerSum(lines: lines)
+    print("Sum of possible game IDs:", resultPart1)
+    print("Sum of the power of game sets:", resultPart2)
 } else {
     fatalError("Could not read the file.")
 }
