e181c2a98fbc6fde366c62f90f6ef43800cb6ceb
Rozeri Dilar <rozeri.dilar@gmail.com>
2023-12-11 18:47:45 +0100

    Add Day 8/ Part 2

--- Day8/Day8.playground/Contents.swift
+++ Day8/Day8.playground/Contents.swift
@@ -65,6 +65,49 @@ final class Day08 {
             result[String(parts[0])] = Map(pair: String(parts[1]))
         }
     }
+
+    // 1. Identify all nodes that end with 'A' and initialize your current set with these nodes.
+    // 2. Iterate through the instructions, updating the set of current nodes based on the direction specified (left or right).
+    // 3. After each step, check if all nodes in the current set end with 'Z'. If they do, you've completed the traversal.
+    func part2() -> Int {
+        let maps = getMap(ArraySlice(roadMap.components(separatedBy: .newlines).filter { !$0.isEmpty }))
+
+        let locations = maps.keys.filter { $0.suffix(1) == "A" }
+        let steps = locations.map { countSteps(from: $0, instructions: instructions.map { String($0) }, maps: maps) }
+        let result = steps.reduce(1, lcm(_:_:))
+
+        return result
+    }
+
+    func lcm(_ m: Int, _ n: Int) -> Int {
+        return (m * n) / gcd(m, n)
+    }
+
+    func gcd(_ m: Int, _ n: Int) -> Int {
+        var a: Int = 0
+        var b: Int = max(m, n)
+        var r: Int = min(m, n)
+
+        while r != 0 {
+            a = b
+            b = r
+            r = a % b
+        }
+        return b
+    }
+
+    func countSteps(from start: String, instructions: [String], maps: [String: Map]) -> Int {
+        let count = instructions.count
+        var idx = 0
+        var map = start
+        var steps = 0
+        while map.suffix(1) != "Z" {
+            map = maps[map]!.next(instructions[idx])
+            steps += 1
+            idx = (idx + 1) % count
+        }
+        return steps
+    }
 }
 
 // MARK: - Models
@@ -110,7 +153,9 @@ let filename = "Input"
 if let fileContent = readFileContent(filename: filename) {
     let day08 = Day08(input: fileContent)
     let resultPart1 = day08.part1()
+    let resultPart2 = day08.part2()
     print("Total step (Part 1):", resultPart1)
+    print("Simultaneous movement from multiple starting nodes step count (Part 2):", resultPart2)
 } else {
     fatalError("Could not read the file.")
 }
